0e7a71329d6e28179aafbcee1146729e67358e21
karolinacicha97 <karolinacicha97@gmail.com>
2022-03-17 12:25:45 +0100

    Polecenie 3.4 testy

--- src/Trygonometria/trygonometria.cpp
+++ src/Trygonometria/trygonometria.cpp
@@ -1,22 +1,27 @@
+#define _USE_MATH_DEFINES
+
 #include "trygonometria.h"
 #include <cmath>
+#include <numbers> // std::numbers
+
+
 double degreemath::sin(double degree) {
-double radian = degree*(3.14*180);
+double radian = degree*(M_PI /180);
 return std::sin(radian); 
 }
 
 double degreemath::cos(double degree) {
-double radian = degree*(3.14*180);
+double radian = degree*(M_PI /180);
 return std::cos(radian);
 }
 
 double degreemath::tg(double degree) {
-double radian = degree*(3.14*180);
+double radian = degree*(M_PI /180);
 return std::tan(radian); 
 }
 
 double degreemath::ctg(double degree) {
-double radian = degree*(3.14*180);
+double radian = degree*(M_PI /180);
 return 1/tan(radian);
 }
 //i tak dalej
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,28 @@
 #include <iostream>
 
 
-int main() {
+int main(int argc, char ** argv) {
 
 
 std::cout << "SINUS\n";
 double result;
 #ifdef USE_TRIGONOMETRY_DEGREE
 	result = degreemath::sin(100);
-	std::cout << "\n By USE_TRIGONOMETRY_DEGREE - STOPNIE";
+	std::cout << "\n By USE_TRIGONOMETRY_DEGREE - STOPNIE\n";
 #else
 	 result = cos(3.14/4.0);
-	 std::cout << "\n By cmath Radiany ";
+	 std::cout << "\n By cmath Radiany\n ";
 #endif
 	
-std::cout <<result;
+//std::cout <<result;
 
+if (argc > 1)
+{
+	
+	double argument = std::atoi(argv[1]);
+	 result = degreemath::sin(argument);
+
+	 std::cout << result<<"\n";
+}
 
 }
